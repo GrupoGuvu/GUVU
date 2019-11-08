5f2f1321a95dc6dad9a98d833455fc3d4f1ab8d5
GrupoGuvu <grupoguvu@gmail.com>
2019-11-07 20:41:40 -0400

    Primer Commit

--- views/main/index.php
+++ views/main/index.php
@@ -11,7 +11,7 @@
 	<?php require 'views/header.php'; ?>
 
 	<div id="main">
-		<h1 class="center">Bienvenido al sitio</h1>
+		<h1 class="center">Bienvenido al sitio Carlos</h1>
 	</div>
 
 	<?php require 'views/footer.php'; ?>
